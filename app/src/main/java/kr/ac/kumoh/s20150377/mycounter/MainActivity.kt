1a1a6be7b440334f39a10fd312cefc1e755d25bf
김하빈 <m16khb@gmail.com>
2019-09-09 17:33:28 +0900

    2019-09-09 17:33 first commit

--- app/src/main/java/kr/ac/kumoh/s20150377/mycounter/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20150377/mycounter/MainActivity.kt
@@ -2,11 +2,23 @@ package kr.ac.kumoh.s20150377.mycounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textNum.text = "0"
+        var i = 0
+        buttonAdd.setOnClickListener{
+            i += 1
+            textNum.text = i.toString()
+        }
+        buttonSub.setOnClickListener{
+            i -= 1
+            textNum.text = i.toString()
+        }
     }
 }
